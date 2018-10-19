15f92821998cbabdcd23fbbaa9098f4f6bcf4966
Tonatiuj Sanchez <tonsanzjimz@gmail.com>
2018-10-19 17:34:54 -0500

    primera prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    constructor( private meta: Meta){
+        meta.addTag({name:'keywords', content:'ng6seo - Version 2 metadatos'});
+        meta.addTag({name:'description', content:'Esta es una version de prueba para los metadatos con Angular 6'});
+        meta.addTag({name:'image', content:'https://c8.alamy.com/comp/F5HYRW/programing-coding-script-technology-website-program-concept-F5HYRW.jpg'});
+    }
 }
